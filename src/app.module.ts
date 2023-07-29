01e866e82c1cd742106c5438c41493422026e45b
Vitaliy Havrona <vitaliyhavrona@gmail.com>
2023-07-29 12:23:39 +0300

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.HOST,
+      port: 3306,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DB,
+      models: [],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cors from 'cors';
+async function start() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type'],
+      credentials: true,
+    }),
+  );
+
+  await app.listen(PORT, () =>
+    console.log(`Server is now going on port ${PORT}`),
+  );
+}
+start();
