41e4c47490f5481c5903e7bbb7dc93acf2f08912
Vitaliy Havrona <vitaliyhavrona@gmail.com>
2023-08-13 14:55:43 +0300

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function start() {
     cors({
       origin: '*',
       methods: ['GET', 'POST'],
-      allowedHeaders: ['Content-Type'],
+      allowedHeaders: ['Content-Type','Authorization'],
       credentials: true,
     }),
   );
